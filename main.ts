21586ffcb99d90a39319795d9a7db7ceee208e81
Vineet <vineetsai@gmail.com>
2020-07-26 17:20:55 -0500

    Show Temperature

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     basic.showString("A")
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(input.temperature())
+})
 input.onButtonPressed(Button.B, function () {
     basic.showString("B")
 })
